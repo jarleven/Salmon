a90dd8c49ba7b86fc5d574ffbce865db2f91afe6
Jarl Even Englund <jarleven@gmail.com>
2018-09-03 23:01:59 +0200

    Create folder based on file timestamp and move the file

---
+++ scripts/movefiles.sh
@@ -0,0 +1,10 @@
+#§/bn/bash
+
+find  /media/tredea/4ae385b1-aa64-462f-8e40-093a1233a92c/Laksen/IPCam/Live2/*.mp4 -type f -mmin +1440 -print0 | while read -d $'\0' file
+do
+	date=$(stat -c %y $file | awk -F" " '{print $1}')
+	echo $date
+	mkdir /media/tredea/4ae385b1-aa64-462f-8e40-093a1233a92c/Laksen/IPCam/$date
+	mv $file  /media/tredea/4ae385b1-aa64-462f-8e40-093a1233a92c/Laksen/IPCam/$date/
+done
+
