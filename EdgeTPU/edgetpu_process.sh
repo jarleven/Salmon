29096a0f567fba4d64559e58ef93e553d1e423af
Jarl Even Englund <jarleven@gmail.com>
2019-06-09 21:45:47 +0200

    Glue for the motion detection and the image classification

---
+++ EdgeTPU/edgetpu_process.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+folder="$1""ready_*"
+
+
+process_folder=$(ls -t1 -d $folder | head -1)
+
+# Will process the oldest folder named ready_foo in the path passed to the script.
+# The folder will be renamed processed_foo when done
+# We are assuming that the previous layer "background subtration" will name folders new_foo and rename them to ready_foo when done writing all the JPGs
+
+
+if [ -z "$process_folder" ]
+then
+      echo "Nothing to do, exiting"
+      exit
+fi
+
+echo "Will run Inception on  ["$process_folder"]"
+
+pathname=$(dirname $process_folder)
+foldername=$(basename $process_folder)
+logname=$process_folder"/logfile.log"
+
+touch $logname
+
+echo $process_folder > $logname
+
+
+# Run classify all the images in the folder
+python3 ~/Salmon/EdgeTPU/classify_folder.py --model ~/EdgeTPU_Models/inception_v1_224_quant_edgetpu.tflite --labels ~/EdgeTPU_Models/imagenet_labels.txt --folder $process_folder --logfile $logname
+
+
+
+# mv $process_folder $pathname"/done_"${foldername/ready_/}
+
---
+++ EdgeTPU/install-EdgeTPU.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+
+wget https://dl.google.com/coral/canned_models/mobilenet_v2_1.0_224_inat_bird_quant_edgetpu.tflite \
+https://dl.google.com/coral/canned_models/inat_bird_labels.txt \
+https://coral.withgoogle.com/static/docs/images/parrot.jpg
+
+exit
+
+
+cd ~/
+
+wget https://dl.google.com/coral/edgetpu_api/edgetpu_api_latest.tar.gz -O edgetpu_api.tar.gz --trust-server-names
+
+tar xzf edgetpu_api.tar.gz
+
+cd edgetpu_api
+
+bash ./install.sh
+
+
+
