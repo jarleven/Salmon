bf4f8f7558fda983a4770fd4334b617df018f8cc
Jarl Even Englund <jarleven@gmail.com>
2019-10-23 19:24:40 +0200

    EdgeTPU

--- EdgeTPU/install-EdgeTPU.sh
+++ EdgeTPU/install-EdgeTPU.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y libedgetpu1-std
+
+
+
 mkdir ~/EdgeTPU_Models
 cd ~/EdgeTPU_Models
 
