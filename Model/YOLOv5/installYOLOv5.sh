ff309fcf0e2c885be43ddda9fc60f8c5e828a22c
jarleven <jarleven@gmail.com>
2022-06-20 21:35:05 +0200

    Create installYOLOv5.sh

---
+++ Model/YOLOv5/installYOLOv5.sh
@@ -0,0 +1,9 @@
+apt update
+#apt-get install ffmpeg libsm6 libxext6  -y
+python -m pip install --upgrade pip
+
+git clone https://github.com/ultralytics/yolov5
+cd yolov5
+pip install -r requirements.txt
+
+python -m pip install opencv-python-headless  # ImportError: libGL.so.1: cannot open shared object file: No such file or directory
