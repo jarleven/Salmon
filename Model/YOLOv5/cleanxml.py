f81679e9cb22ee0b680524f6be29d0fc9fca74db
jarleven <jarleven@gmail.com>
2022-07-09 23:33:48 +0200

    Update cleanxml.py

--- Model/YOLOv5/cleanxml.py
+++ Model/YOLOv5/cleanxml.py
@@ -1,14 +1,10 @@
 # import required module
 from pathlib import Path
-#from pathlib import PureWindowsPath
 
 import os.path
 
- 
 # get the path/directory
-#folder_dir = 'D:\Git\Salmon\Dataset\Autodetect\'
 #folder_dir = Path('D:\\Git\\Salmon\\Dataset\\Autodetect\\')
-
 folder_dir = Path(r'D:\tmp\full\2022-06-29')
  
 print(folder_dir)
@@ -21,7 +17,6 @@ for xml in xmls:
     #print(xml)
     jpgfile = (Path(xml).parent.absolute() / Path(xml).stem).with_suffix('.jpg')
 
-    #jpgfile = str(Path(xml).parent.absolute())+"\\"+str(Path(xml).stem)+".jpg"
     #print(jpgfile)
     if not os.path.exists(jpgfile):
         print("Warning")
@@ -31,13 +26,10 @@ jpgs = Path(folder_dir).glob('*.jpg')
 for jpg in jpgs:
     #print(jpg)
     xmlfile = (Path(jpg).parent.absolute() / Path(jpg).stem).with_suffix('.xml')
-
-    #xmlfile = str(Path(jpg).parent.absolute())+"\\"+str(Path(jpg).stem)+".xml"
     #print(xmlfile)
     if not os.path.exists(xmlfile):
         print("move", jpg, "D:\\cleanup")
         #copyfile(source, destination)
-
 
         #  MOVE THE FILE Path.unlink(xml) 
 
